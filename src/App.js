ac7f2fe33fc0deacb887976b3c077df491a80f03
Ben Gosney <bengosney@googlemail.com>
2019-04-12 09:03:16 +0100

    Redraw on resize

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
 	const length = Math.floor(innerWidth / 2);
 	const noise = new Noise(length, [-range, range]);
 	
-	this.setState({ width: innerWidth, height: innerHeight, noise: noise, length: length });
+	this.setState({ width: innerWidth, height: innerHeight, noise: noise, length: length }, () => this.nextFrame());
     }
     
     componentWillUnmount() {
